8722867dffff2a215d63a837ff97455c2ef3911d
Asko Kauppi <demo@outstanding.earth>
2024-08-22 16:07:34 +0300

    interim; 'i2c_nudge' does not build

--- examples/a.rs
+++ examples/a.rs
@@ -12,13 +12,6 @@ use core::{
     ptr::NonNull
 };
 use esp_backtrace as _;
-#[cfg(feature = "dingdong")]
-use esp_hal::{
-    clock::ClockControl,
-    delay::Delay,
-    peripherals::Peripherals,
-    system::SystemControl,
-};
 
 use esp_hal::prelude::*;
 
@@ -29,24 +22,6 @@ use rustyard::{Context, tunnel, Platform};
 #[entry]
 fn main() -> ! {
     info!("Hello");
-
-    #[cfg(feature = "dingdong")]
-    {
-        let peripherals = Peripherals::take();
-        let system = SystemControl::new(peripherals.SYSTEM);
-        let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
-        let d_provider = Delay::new(&clocks);
-        let delay_ms: _ = {     // synchronous (busy wait) delays
-            |ms| { d_provider.delay_millis(ms); }
-        };
-
-        let mut ding: bool = true;
-        loop {
-            delay_ms(1000);
-            debug!("{}", if ding {"Ding!"} else {"Dong!"} );
-            ding = !ding;
-        }
-    }
 
     let mp: *mut Context = {
         static X: StaticCell<RustPlatform> = StaticCell::new();
--- examples/i2c_nudge.rs
+++ examples/i2c_nudge.rs
@@ -15,24 +15,17 @@
 use defmt::{debug, info, error};
 use defmt_rtt as _;
 
-use embedded_hal::i2c::{
-    //I2c,
-    Error as I2cError,
-    Operation as I2cOperation
-};
-
 use esp_backtrace as _;
 use esp_hal::{
     clock::ClockControl,
     gpio::Io,
     i2c::{I2C, Error as I2CError},
     delay::Delay,
-    peripherals::Peripherals,
+    peripherals::{I2C0, Peripherals},
     prelude::*,
     system::SystemControl,
-    Blocking
+    Blocking,
 };
-use esp_hal::peripherals::I2C0;
 use static_cell::StaticCell;
 
 #[entry]
@@ -51,6 +44,7 @@ fn main() -> ! {
         &clocks,
         None
     );
+    let mut i2c = MyI2C::new(I2C_ADDR, i2c);
 
     let delay_ms = {
         static D: StaticCell<Delay> = StaticCell::new();
@@ -70,32 +64,68 @@ fn main() -> ! {
     }
 }
 
+type MyI2C = I2C<'_,I2C0,Blocking>;
+
 // Pings the ST.com VL53L5CX time-of-flight sensor.
 //
 const I2C_ADDR: u8 = 0x52 >> 1;
 
-fn vl_is_alive(i2c: &mut I2C<I2C0,Blocking>) -> Result<[u8;2],I2cError>
+fn vl_is_alive(i2c: &mut MyI2C) -> Result<[u8;2],I2CError>
 {
     let mut buf: [u8; 2] = [0; 2];
 
-    let rd = |addr: u16, buf: &mut [u8]| -> Result<(),_> {
-        let addr: [u8;2] = addr.to_be_bytes();
-        i2c.write_read(I2C_ADDR, &addr, buf)
-    };
-
-    // '.transaction' taken from how 'embedded_hal' implements '.write_read()'.
-    //
-    // tbd. If there's a way to '[addr, *vs].concat()' (concat slice-of-slices to a slice),
-    //      we don't need to do the '.transaction' (and don't need to specifically import 'embedded_hal').
-    let wr = |addr: u16, vs: &[u8]| -> Result<(),_> {
-        let addr: [u8;2] = addr.to_be_bytes();
-
-        i2c.transaction(I2C_ADDR, &mut [I2cOperation::Write(&addr), I2cOperation::Write(vs)])
-    };
-
-    wr(0x7fff, &[0])?;
-    rd(0, &mut buf)?;            // rd indices 0,1 (device id, rev id); should give (0xf0, 0x02)
-    wr(0x7fff, &[2])?;
+    i2c.wr(0x7fff, &[0]);
+    i2c.rd(0, &mut buf);       // indices 0,1 are (device id, rev id); should give (0xf0, 0x02)
+    i2c.wr(0x7fff, &[2]);
 
     Ok(buf)
 }
+
+// Return codes were a nightmare (never got them right!) so instead panicking inside these two. #giving-up
+//
+fn rd(&mut i2c: MyI2C, index: u16, buf: &mut [u8]) /*-> Result<(),I2CError>*/ {
+    i2c.write_read(I2C_ADDR, &index.to_be_bytes(), buf)
+        .ok();
+}
+
+// Q1: no 'write_write' in 'esp-hal::i2c::I2C'
+// Q2: any way to concatenate slices (of unknown size) together, in 'no_std'?
+//
+fn wr(&mut i2c: MyI2C, index: u16, vs: &[u8;1]) /*-> Result<(),I2CError>*/ {
+    let index: [u8;2] = index.to_be_bytes();
+    let data: [u8;3] = [index[0], index[1], vs[0]];
+    i2c.write(I2C_ADDR, &data).ok();
+}
+
+/*** reserve
+trait I2C_WW {
+    fn write_write(&mut self, addr: u8, prelude: &[u8;2], bytes: &[u8]) -> Result<(), I2C::Error>;
+}
+impl I2C_WW for I2C<I2C0,Blocking> {
+    // For symmetry with 'write_read' - ideas are welcome!!!
+    //
+    fn write_write(&mut self, addr: u8, prelude: &[u8;2], bytes: &[u8]) -> Result<(), I2C::Error> {
+
+        // #hack Since we don't know how to concatenate '[u8]'s in 'no_std', we support only the
+        //      1-byte long case.
+        //
+        #[cfg(not(feature = "embedded-hal"))]
+        let wr = |index: u16, vs: &[u8;1]| -> Result<(),_> {
+            let index: [u8;2] = index.to_be_bytes();
+            let data: [u8;3] = [index[0], index[1], vs[0]];
+            i2c.write(I2C_ADDR, &data)
+        };
+
+        // '.transaction' taken from how 'embedded_hal' implements '.write_read()'.
+        //
+        // tbd. If there's a way to '[addr, *vs].concat()' (concat slice-of-slices to a slice),
+        //      we don't need to do the '.transaction' (and don't need to specifically import 'embedded_hal').
+        #[cfg(feature = "embedded-hal")]
+        let wr = |addr: u16, vs: &[u8]| -> Result<(),_> {
+            let addr: [u8;2] = addr.to_be_bytes();
+            i2c.transaction(I2C_ADDR, &mut [I2cOperation::Write(&addr), I2cOperation::Write(vs)])
+        };
+
+    }
+}
+***/
